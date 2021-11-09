44b3bedd1bcb017aa9c79d2d2dcdd7f0c85af1da
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2021-11-09 14:20:53 -0600

    I updated my search and buttons. My first version of displaying my info

--- script.js
+++ script.js
@@ -9,17 +9,42 @@ const unova = document.querySelector(".unova");
 const kalos = document.querySelector(".kalos");
 const alola = document.querySelector(".alola");
 const galar = document.querySelector(".galar");
+const test = document.querySelector("#test")
 
-kanto.addEventListener('click', getKantoPokemon);
+
+
+function showKantoPokemon(pokeNames) {
+  console.log(pokeNames);
+  // pokeNames.forEach((pokeNames) => {
+  //   test.innerText = pokeNames.results[pokeNames].name;
+  // });
+  let listOfPokemon = [];
+  for (let i = 0; i < 150; i++) {
+    listOfPokemon.push(pokeNames.results[i].name);
+  }
+  test.innerText = listOfPokemon;
+
+  //test.innerText = pokeNames.results[0].name;
+
+}
 
 async function getKantoPokemon() {
   try {
-    const res = await axios.get("https://pokeapi.co/api/v2/pokedex/kanto/");
-    console.log(res);
-
+    const res = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151");
+    let pokemonName = res.data;
+    showKantoPokemon(pokemonName);
   } catch (err) {
     alert(err);
   }
 }
 
 getKantoPokemon();
+
+const searchForm = document.querySelector("#pokemon-data");
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("submitted")
+})
+
+kanto.addEventListener('click', getKantoPokemon);
