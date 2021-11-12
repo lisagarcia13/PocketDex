d2e9400e3b3cf702b94a3630c7fd594336d46e01
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2021-11-11 19:46:31 -0600

    new pictures and name

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 // // https://pokeapi.co/api/v2/pokedex/hoenn/
 
-// const kanto = document.querySelector(".kanto");
-// const johto = document.querySelector(".johto");
-// const hoenn = document.querySelector(".hoenn");
-// const sinnoh = document.querySelector(".sinnoh");
-// const unova = document.querySelector(".unova");
-// const kalos = document.querySelector(".kalos");
-// const alola = document.querySelector(".alola");
-// const galar = document.querySelector(".galar");
+const kanto = document.querySelector(".kanto");
+const johto = document.querySelector(".johto");
+const hoenn = document.querySelector(".hoenn");
+const sinnoh = document.querySelector(".sinnoh");
+const unova = document.querySelector(".unova");
+const kalos = document.querySelector(".kalos");
+const alola = document.querySelector(".alola");
+const galar = document.querySelector(".galar");
 // const test = document.querySelector("#test");
 // const pokeName = document.querySelector(".kanto-name");
 
@@ -54,93 +54,31 @@
 //   test.appendChild(k);
 // }
 
-// async function kantoAbilities() {
-//   try {
-//     const res = await axios.get("https://pokeapi.co/api/v2/pokemon/2/");
-//     let moves = res.data.abilities;
-//     displayKantoAbilities(moves);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-// kantoAbilities();
-
-// function displayKantoAbilities(moves) {
-//   console.log(moves);
-//   let pokeAbilities = document.createElement("p");
-//   pokeAbilities.innerText = moves[0].ability.name;
-//   test.appendChild(pokeAbilities);
-// }
-
-// async function kantoTypes() {
-//   try {
-//     const res = await axios.get("https://pokeapi.co/api/v2/pokemon/2/");
-//     let pokeTypes = res.data.types;
-//     displayKantoTypes(pokeTypes);
-
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-// kantoTypes();
-
-// function displayKantoTypes(pokeTypes) {
-//   console.log(pokeTypes)
-//   let infoType = document.createElement("p");
-//   infoType.innerText = pokeTypes[0].type.name;
-//   test.appendChild(infoType);
-// }
 
 
 
 
-// const searchForm = document.querySelector("#pokemon-data");
-// const searchText = document.querySelector("#search");
+
+
+// Promise.all(emptyArray).then((results) => {
+// const kantoPokemon = results.map((kantoPokemonName) => ({
 
 
 
-// searchForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   test.innerHTML = "";
-//   pokeName.innerHTML = "";
-//   const searchPokemon = searchText.value
-//   getKantoPokemon(searchPokemon);
-
-// })
-
-// kanto.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   test.innerHTML = "";
-//   pokeName.innerHTML = "";
-//   getKantoPokemon();
+//   }));
+//   console.log(kantoPokemon);
 // });
 
 const pokeInfo = document.querySelector(".pokemon-info")
+const displayPokemon = document.querySelector(".container");
 
 async function getKantoPokemon() {
-  const emptyArray = [];
-  for (let i = 1; i < 151; i++) {
+  for (let i = 1; i <= 151; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`
     try {
       const res = await axios.get(url);
       let kantoPokemonName = res.data;
-      Promise.all(emptyArray).then((results) => {
-        const kantoPokemon = results.map((kantoPokemonName) => ({
-          name: kantoPokemonName.name,
-          id: kantoPokemonName.id,
-          image: kantoPokemonName.sprites["front_default"],
-          ability: kantoPokemonName.abilities.map((ability) => ability.ability.name).join(','),
-          type: kantoPokemonName.types.map((type) => type.type.name).join(','),
-
-
-        }));
-        console.log(kantoPokemon);
-        emptyArray.push(kantoPokemon);
-
-      });
       showKantoPokemon(kantoPokemonName);
-
-
     } catch (err) {
       alert(err);
     }
@@ -151,15 +89,37 @@ getKantoPokemon();
 
 function showKantoPokemon(kantoPokemonName) {
   console.log(kantoPokemonName);
-
-
-  // const kantoPokemonDisplay = KantoPokemonResults.map((pokemons) => `
-  // <li>
-  // <img src ="${pokemons.image}" />
-  // <h3> ${pokemons.id}. ${pokemons.name} </h3>
-  // <p> Type: ${pokemons.type} </p>
-  // </li>
-  // `
-  // );
-  // pokeInfo.innerHTML = kantoPokemonDisplay;
+  // const kantoPokemon = {
+  //   name: kantoPokemonName.name,
+  //   id: kantoPokemonName.id,
+  //   image: kantoPokemonName.sprites["front_default"],
+  //   ability: kantoPokemonName.abilities.map((ability) => ability.ability.name).join(','),
+  //   type: kantoPokemonName.types.map((type) => type.type.name).join(',')
+  // }
+  // console.log(kantoPokemon);
+  const pokemonDiv = document.createElement("div");
+  pokemonDiv.innerHTML = ` 
+  <img src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${kantoPokemonName.id}.png" />
+  <h3> ${kantoPokemonName.id}. ${kantoPokemonName.name} </h3>
+  `
+  displayPokemon.appendChild(pokemonDiv);
 }
+
+const searchForm = document.querySelector("#pokemon-data");
+const searchText = document.querySelector("#search");
+
+
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  displayPokemon.innerHTML = "";
+  const searchPokemon = searchText.value
+  getKantoPokemon(searchPokemon);
+
+})
+
+kanto.addEventListener('click', (e) => {
+  e.preventDefault();
+  displayPokemon = ""
+  getKantoPokemon();
+});
