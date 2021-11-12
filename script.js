c90a56206a4b1f9f6c975249dda28329d6550096
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2021-11-12 09:32:43 -0600

    Region buttons working. Search in progress

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-// // https://pokeapi.co/api/v2/pokedex/hoenn/
-
 const kanto = document.querySelector(".kanto");
 const johto = document.querySelector(".johto");
 const hoenn = document.querySelector(".hoenn");
@@ -10,69 +8,115 @@ const kalos = document.querySelector(".kalos");
 const alola = document.querySelector(".alola");
 const galar = document.querySelector(".galar");
 
-
-const pokeInfo = document.querySelector(".pokemon-info")
+const pokeInfo = document.querySelector(".pokemon-info");
 const displayPokemon = document.querySelector(".container");
+//Object Pokemon
+const Pokemon = {
+  id: 0,
+  name: "",
+  types: [],
+  abilities: [],
+  bio: ""
+};
+//Array of all Object: Pokemon
+const AllPokemon = [];
 
-async function getKantoPokemon() {
-  for (let i = 1; i <= 151; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`
+//Gets Kanto Pokemon data from [1-151]
+async function getPokemonFromRange(start, end) {
+  console.log(`LOG: getPokemonFromRange(${start}, ${end})`)
+  for (let i = start; i <= end; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     try {
       const res = await axios.get(url);
-      let kantoPokemonName = res.data;
-      showKantoPokemon(kantoPokemonName);
+      //AllPokemon.push(res.data); //Implement later
+      showPokemon(res.data)
     } catch (err) {
       alert(err);
     }
   }
 }
-
-getKantoPokemon();
-
-function showKantoPokemon(kantoPokemonName) {
-  console.log(kantoPokemonName);
+//Displays Pokemon with response data.id
+function showPokemon(pokemon) {
+  console.log(`LOG: showPokemon(${pokemon})`);
   const pokemonDiv = document.createElement("div");
-  pokemonDiv.innerHTML = ` 
-  <img src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${kantoPokemonName.id}.png" />
-  <h3> ${kantoPokemonName.id}. ${kantoPokemonName.name} </h3>
-  `
+  pokemonDiv.innerHTML = `<img src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
+  <h3> ${pokemon.id} ${pokemon.name} </h3>`;
   displayPokemon.appendChild(pokemonDiv);
 }
 
-//divided into separate functions
-// called this function in my HTML
-function kantoButton() {
-  if (displayPokemon.style.display === 'none') {
-    displayPokemon.style.display = 'block';
-  } else {
-    displayPokemon.style.display = 'none';
+//Searches for a Pokemon in all Pokemon
+async function searchPokemon(findme) {
+  console.log(`LOG: searchPokemon(${findme})`);
+  const url = `https://pokeapi.co/api/v2/pokedex/national/`; //using national dex for pokemon lookup
+  try {
+    const res = await axios.get(url);
+    let pokeball = res.data.pokemon_entries.filter(function (obj) { return searchObj(obj, findme) }); //returns either object of a pokemon OR an empty array pokemon not found.
+    if (pokeball.length !== 0) { //If object: "found" is larger than 0, then pokemon was found.
+      console.log("Search Successful.");
+      let pokemonID = pokeball[0].entry_number; //object uses national pokedex to return object with ID and name. 
+      getPokemonFromRange(pokemonID, pokemonID); //uses function to lookup pokemon by ID. 
+    } else { //if array is 
+      console.log("Search Failed. Try Again.");
+    }
+  } catch (err) {
+    alert(err);
   }
+}
+
+
+// Search in progress
+function searchObj(obj, query) {
+  for (var key in obj) {
+    var value = obj[key];
+    if (typeof value === 'object') {
+      return searchObj(value, query);
+    }
+    if (typeof value === 'string' && value.toLowerCase().indexOf(query.toLowerCase())) {
+      return obj;
+    }
+  }
+}
+
+function Gen1Button() {
+  getPokemonFromRange(1, 151);
+  displayPokemon.style.display = 'block';
+}
+function Gen2Button() {
+  getPokemonFromRange(152, 251);
+  displayPokemon.style.display = 'block';
+}
+function Gen3Button() {
+  getPokemonFromRange(252, 386);
+  displayPokemon.style.display = 'block';
+}
+function Gen4Button() {
+  getPokemonFromRange(387, 493);
+  displayPokemon.style.display = 'block';
+}
+function Gen5Button() {
+  getPokemonFromRange(494, 649);
+  displayPokemon.style.display = 'block';
+}
+function Gen6Button() {
+  getPokemonFromRange(650, 721);
+  displayPokemon.style.display = 'block';
+}
+function Gen7Button() {
+  getPokemonFromRange(722, 809);
+  displayPokemon.style.display = 'block';
+}
+function Gen8Button() {
+  getPokemonFromRange(810, 898);
+  displayPokemon.style.display = 'block';
 }
 const searchForm = document.querySelector("#pokemon-data");
 const searchText = document.querySelector("#search");
 
 
-
+// In progress
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   displayPokemon.innerHTML = "";
-  const searchPokemon = searchText.value
-  getKantoPokemon(searchPokemon);
-
+  const input = searchText.value;
+  searchPokemon(input);
 })
-
-
-// kanto.addEventListener('click', (e) => {
-//   e.preventDefault();
-
-//   // displayPokemon.innerHTML = "";
-// });
-
-// try for search bar
-// function removeKantoPokemon() {
-//   const removeElement = document.querySelector(".container");
-//   while (removeElement.lastChild) {
-//     removeElement.removeChild(removeElement.lastChild);
-//   }
-// }
-// removeKantoPokemon();
